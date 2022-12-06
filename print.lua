4fb4c737ef41a16dc2efd846a72ce219fa21d94e
Jarxay <99241070+Jarxay@users.noreply.github.com>
2022-12-06 15:56:25 +0100

    Update print.lua

--- print.lua
+++ print.lua
@@ -2,8 +2,6 @@
 -- Version: 3.2
 
 -- Instances:
-getgenv().text = "BLAXINATOR"
-loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/gamesensewatermark.lua")()
 
 local ExecutorUsing = is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or secure_load and "Sentinel" or KRNL_LOADED and "Krnl" or SONA_LOADED and "Sona" or "WTF?"
 local HttpService = game:GetService("HttpService")
@@ -174,148 +172,6 @@ local function NJVPQ_fake_script() -- TextButton.LocalScript
 			Duration = 0.1;
 			--Callback here
 		})
-		local function skeelesp()
-			local camera = workspace.CurrentCamera
-			local entities = game:GetService("Players")
-			local localplayer = entities.LocalPlayer 
-			local runservice = game:GetService("RunService")
-
-			local esp_settings = {
-				enabled = true,
-				skel = true,
-				skel_col = Color3.fromRGB(0,255,255)
-			}
-
-			local function draw(player, character)
-
-				local skel_head = Drawing.new("Line")
-				skel_head.Visible = false
-				skel_head.Thickness = 1.5
-				skel_head.Color = Color3.new(1,1,1)
-
-				local skel_torso = Drawing.new("Line")
-				skel_torso.Visible = false
-				skel_torso.Thickness = 1.5
-				skel_torso.Color = Color3.new(1,1,1)
-
-				local skel_leftarm = Drawing.new("Line")
-				skel_leftarm.Visible = false
-				skel_leftarm.Thickness = 1.5
-				skel_leftarm.Color = Color3.new(1,1,1)
-
-				local skel_rightarm = Drawing.new("Line")
-				skel_rightarm.Visible = false
-				skel_rightarm.Thickness = 1.5
-				skel_rightarm.Color = Color3.new(1,1,1)
-
-				local skel_leftleg = Drawing.new("Line")
-				skel_leftleg.Visible = false
-				skel_leftleg.Thickness = 1.5
-				skel_leftleg.Color = Color3.new(1,1,1)
-
-				local skel_rightleg = Drawing.new("Line")
-				skel_rightleg.Visible = false
-				skel_rightleg.Thickness = 1.5
-				skel_rightleg.Color = Color3.new(1,1,1)
-
-				local function update()
-					local connection
-					connection = runservice.RenderStepped:Connect(function()
-
-						if workspace:FindFirstChild(character.Name) and
-							character and 
-							character:FindFirstChild("HumanoidRootPart") and 
-							character:FindFirstChild("Humanoid") and 
-							character:FindFirstChild("Humanoid").Health ~= 0 then 
-
-							local character_rootpart_3d = character.HumanoidRootPart.Position
-							local character_rootpart_2d, rootpart_onscreen = camera:WorldToViewportPoint(character_rootpart_3d)
-
-							if rootpart_onscreen and character.Humanoid.RigType == Enum.HumanoidRigType.R6 and esp_settings.enabled then
-
-								local head_2d = camera:WorldToViewportPoint(character.Head.Position)
-								local torso_upper_2d = camera:WorldToViewportPoint(character.Torso.Position + Vector3.new(0,1,0))
-								local torso_lower_2d = camera:WorldToViewportPoint(character.Torso.Position + Vector3.new(0,-1,0))
-
-								local leftarm_2d = camera:WorldToViewportPoint(character["Left Arm"].Position + Vector3.new(0,-1,0))
-								local rightarm_2d = camera:WorldToViewportPoint(character["Right Arm"].Position + Vector3.new(0,-1,0))
-								local leftleg_2d = camera:WorldToViewportPoint(character["Left Leg"].Position + Vector3.new(0,-1,0))
-								local rightleg_2d = camera:WorldToViewportPoint(character["Right Leg"].Position + Vector3.new(0,-1,0))
-
-								skel_head.From = Vector2.new(head_2d.X, head_2d.Y)
-								skel_head.To = Vector2.new(torso_upper_2d.X, torso_upper_2d.Y)
-
-								skel_torso.From = Vector2.new(torso_upper_2d.X, torso_upper_2d.Y)
-								skel_torso.To = Vector2.new(torso_lower_2d.X, torso_lower_2d.Y)
-
-								skel_leftarm.From = Vector2.new(torso_upper_2d.X, torso_upper_2d.Y)
-								skel_leftarm.To = Vector2.new(leftarm_2d.X, leftarm_2d.Y)
-
-								skel_rightarm.From = Vector2.new(torso_upper_2d.X, torso_upper_2d.Y)
-								skel_rightarm.To = Vector2.new(rightarm_2d.X, rightarm_2d.Y)
-
-								skel_leftleg.From = Vector2.new(torso_lower_2d.X, torso_lower_2d.Y)
-								skel_leftleg.To = Vector2.new(leftleg_2d.X, leftleg_2d.Y)
-
-								skel_rightleg.From = Vector2.new(torso_lower_2d.X, torso_lower_2d.Y)
-								skel_rightleg.To = Vector2.new(rightleg_2d.X, rightleg_2d.Y)
-
-								skel_head.Visible = esp_settings.skel
-								skel_torso.Visible = esp_settings.skel
-								skel_leftarm.Visible = esp_settings.skel
-								skel_rightarm.Visible = esp_settings.skel
-								skel_leftleg.Visible = esp_settings.skel
-								skel_rightleg.Visible = esp_settings.skel
-
-							else
-
-								skel_head.Visible = false
-								skel_torso.Visible = false
-								skel_leftarm.Visible = false
-								skel_rightarm.Visible = false
-								skel_leftleg.Visible = false
-								skel_rightleg.Visible = false
-
-							end
-
-						else
-
-							if player == nil then
-								connection:Disconnect()
-								connection = nil 
-							end
-
-							skel_head.Visible = false
-							skel_torso.Visible = false
-							skel_leftarm.Visible = false
-							skel_rightarm.Visible = false
-							skel_leftleg.Visible = false
-							skel_rightleg.Visible = false
-
-						end
-					end)
-				end
-				coroutine.wrap(update)()
-
-			end
-
-			local function playeradded(player)
-				if player.Character then
-					coroutine.wrap(draw)(player, player.Character)
-				end
-				player.CharacterAdded:Connect(function(character)
-					coroutine.wrap(draw)(player, character)
-				end)
-			end
-
-			for a,b in next, entities:GetPlayers() do
-				if b ~= localplayer then
-					playeradded(b)
-				end
-			end
-
-			entities.PlayerAdded:Connect(playeradded)
-		end
 		local function esp()
 
 			assert(Drawing, "missing dependency: drawing");
@@ -346,22 +202,22 @@ local function NJVPQ_fake_script() -- TextButton.LocalScript
 				local esp = {};
 
 				esp.box = new_drawing("Square", true);
-				esp.box.Color = new_color3(0,255,25);
+				esp.box.Color = new_color3(255,255,0);
 				esp.box.Thickness = 0.8;
 				esp.box.Filled = false;
 
 				esp.tracer = new_drawing("Line", true);
-				esp.tracer.Color = new_color3(0,255,255);
-				esp.tracer.Thickness = 0.5;
+				esp.tracer.Color = new_color3(255,255,0);
+				esp.tracer.Thickness = 0.25;
 
 				esp.name = new_drawing("Text", true);
-				esp.name.Color = new_color3(0,255,255);
-				esp.name.Size = 15;
+				esp.name.Color = new_color3(255,255,0);
+				esp.name.Size = 1;
 				esp.name.Center = true;
 
 				esp.distance = new_drawing("Text", true);
-				esp.distance.Color = new_color3(0,255,255);
-				esp.distance.Size = 15;
+				esp.distance.Color = new_color3(255,255,0);
+				esp.distance.Size = 16;
 				esp.distance.Center = true;
 
 				cache[player] = esp;
@@ -385,25 +241,25 @@ local function NJVPQ_fake_script() -- TextButton.LocalScript
 
 							esp.box.Visible = visible;
 							esp.tracer.Visible = visible;
-							esp.name.Visible = visible;
+							esp.name.Visible = false;
 							esp.distance.Visible = visible;
 
 							if visible then
 								local scale_factor = 1 / (position.Z * tan(rad(camera.FieldOfView * 0.5)) * 2) * 100;
-								local width, height = floor(35 * scale_factor), floor(50 * scale_factor);
+								local width, height = floor(45 * scale_factor), floor(70 * scale_factor);
 								local x, y = floor(position.X), floor(position.Y);
 
 								esp.box.Size = new_vector2(width, height);
-								esp.box.Position = new_vector2(floor(x - width * 0.5), floor(y - height * 0.5));
+								esp.box.Position = new_vector2(floor(x - width * 0.5), floor(y - height * 0.4));
 
 								esp.tracer.From = new_vector2(floor(viewport_size.X * 0.5), floor(viewport_size.Y * 0.5));
-								esp.tracer.To = new_vector2(x, floor(y + height * 0.5));
+								esp.tracer.To = new_vector2(x, floor(y + height * 0.1));
 
 								esp.name.Text = player.Name;
-								esp.name.Position = new_vector2(x, floor(y - height * 0.5 - esp.name.TextBounds.Y));
+								esp.name.Position = new_vector2(x, floor(y - height * 0.7 - esp.name.TextBounds.Y));
 
-								esp.distance.Text = "["..floor(position.Z) .. "]";
-								esp.distance.Position = new_vector2(x, floor(y + height * 0.5));
+								esp.distance.Text = floor(position.Z) .. " studs";
+								esp.distance.Position = new_vector2(x, floor(y + height * -0.8));
 							end
 
 						else if player.Character.Humanoid.Health <= 1 then
